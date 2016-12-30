992a5be3ea01bf90c51703b44d15bd16aebd3c44
Maurice Lepouttre <maurice@jwplayer.com>
2016-12-29 20:07:09 -0500

    added things-to-do page

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,12 @@ router.get('/info', function(req, res, next) {
   res.render('info');
 });
 
+router.get('/accomodations', function(req, res, next) {
+  res.render('accomodations');
+});
+
+router.get('/things-to-do', function(req, res, next) {
+  res.render('things-to-do');
+});
+
 module.exports = router;
